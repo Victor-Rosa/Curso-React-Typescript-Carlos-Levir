b85af556855916f30bfe3fde0496a65d99c22a55
Victor Rosa <victorsouzarosa100@gmail.com>
2021-09-11 12:15:24 -0300

    Add TS

--- TS/index.ts
+++ TS/index.ts
@@ -111,3 +111,53 @@ const newGithubUser: GithubUserData = {
 }
 
 newGithubUser.following = '200'
+
+
+//Generic:  são funcionalidades que permitem a passagem de tipos
+
+// State - S
+// Type - T
+// Key - K
+// Value - V
+// Element - E
+
+
+function print <T> (params: T){
+    console.log(params);
+}
+
+function printNumber (params: number){
+    print<number>(params);
+}
+
+print<string>('text')
+
+function useState <S extends  number | string = string>(initialState?: S){
+    let state =  initialState
+
+    function setState(newState: S){
+        state = newState;
+
+        return state;
+    }
+
+    return {state, setState};
+}
+
+const state = useState<number>(15) // Number porque eu defini que vou passar para função com um number
+const state1 = useState(105) //String porque eu defini que os parametros passados para função serão string se eles não forem informados.
+
+
+// passando funções como parâmetros
+
+type Callback = (result: string) => void;
+
+function printWind(callback: Callback){
+
+}
+
+function callback(nome: string){
+
+}
+
+printWind(callback);
